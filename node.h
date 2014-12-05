58bb8ea153e2d5f77e19343abfe6f237985d4786
Chandan Challana <chandan.challana@gmail.com>
2014-12-04 16:05:20 -0800

    main.cpp

---
+++ node.h
@@ -0,0 +1,10 @@
+/*The file contains the data structure node that will be used by all the functions
+  that will act on this data structures
+  Attributes:
+  int data
+  struct node* */
+
+  struct node{
+  int data;
+  struct node* next;
+  };
